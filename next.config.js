0fccdab2d746235be7eee834cbb15e9332e8b526
Dipalkumar Bhavsar <dipalkumar.bhavsar@bajajfinserv.in>
2024-12-08 10:20:37 +0530

    change config

--- next.config.js
+++ next.config.js
@@ -1,91 +1,120 @@
-const { withContentlayer } = require('next-contentlayer2')
+// const { withContentlayer } = require('next-contentlayer2')
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
-// You might need to insert additional domains in script-src if you are using external services
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is statichunt.com http://www.youtube.com;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data: statichunt.com;
-  media-src 'self' *.s3.amazonaws.com;
-  connect-src * statichunt.com;
-  font-src 'self';
-  frame-src giscus.app https://www.youtube.com/ https://www.youtube-nocookie.com/
-`;
+// // You might need to insert additional domains in script-src if you are using external services
+// const ContentSecurityPolicy = `
+//   default-src 'self';
+//   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is statichunt.com http://www.youtube.com;
+//   style-src 'self' 'unsafe-inline';
+//   img-src * blob: data: statichunt.com;
+//   media-src 'self' *.s3.amazonaws.com;
+//   connect-src * statichunt.com;
+//   font-src 'self';
+//   frame-src giscus.app https://www.youtube.com/ https://www.youtube-nocookie.com/
+// `;
 
-const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-];
+// const securityHeaders = [
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+//   {
+//     key: 'Content-Security-Policy',
+//     value: ContentSecurityPolicy.replace(/\n/g, ''),
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+//   {
+//     key: 'Referrer-Policy',
+//     value: 'strict-origin-when-cross-origin',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+//   {
+//     key: 'X-Content-Type-Options',
+//     value: 'nosniff',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+//   {
+//     key: 'X-DNS-Prefetch-Control',
+//     value: 'on',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+//   {
+//     key: 'Strict-Transport-Security',
+//     value: 'max-age=31536000; includeSubDomains',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+//   {
+//     key: 'Permissions-Policy',
+//     value: 'camera=(), microphone=(), geolocation=()',
+//   },
+// ];
 
-/**
- * @type {import('next/dist/next-server/server/config').NextConfig}
- **/
-module.exports = () => {
-  const plugins = [withContentlayer, withBundleAnalyzer]
-  return plugins.reduce((acc, next) => next(acc), {
-    reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['app', 'components', 'layouts', 'scripts'],
-    },
-    output: "export",
-    basePath: "/myblog",
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'picsum.photos',
-          pathname: '**',
-        },
-      ],
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: securityHeaders,
-        },
-      ]
-    },
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
+// /**
+//  * @type {import('next/dist/next-server/server/config').NextConfig}
+//  **/
+// module.exports = () => {
+//   const plugins = [withContentlayer, withBundleAnalyzer]
+//   return plugins.reduce((acc, next) => next(acc), {
+//     reactStrictMode: true,
+//     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+//     eslint: {
+//       dirs: ['app', 'components', 'layouts', 'scripts'],
+//     },
+//     output: "export",
+//     basePath: "/myblog",
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: 'picsum.photos',
+//           pathname: '**',
+//         },
+//       ],
+//     },
+//     async headers() {
+//       return [
+//         {
+//           source: '/(.*)',
+//           headers: securityHeaders,
+//         },
+//       ]
+//     },
+//     webpack: (config, options) => {
+//       config.module.rules.push({
+//         test: /\.svg$/,
+//         use: ['@svgr/webpack'],
+//       })
 
-      return config
-    }
-  })
-}
+//       return config
+//     }
+//   })
+// }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/myblog",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
